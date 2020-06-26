1a4c90a7092492a4c61031c805e356cd2cce1efb
lucas trento <lucastrento86@gmail.com>
2020-06-26 04:48:08 -0300

    Initial commit

---
+++ app/src/main/java/com/example/pantalla1/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.pantalla1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnIngresar;
+    private EditText edtNombre, edtPass;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnIngresar = findViewById(R.id.btnIngresar);
+        edtNombre = findViewById(R.id.edtNombre);
+        edtPass = findViewById(R.id.edtPass);
+
+        btnIngresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ingresar();
+            }
+        });
+
+
+
+
+    }
+
+    private void ingresar(){
+
+        String user = edtNombre.getText().toString();
+        String pass = edtPass.getText().toString();
+
+        if(user.equals("ites") && pass.equals("1234")){
+            Toast.makeText(this, "acceso válido", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            Toast.makeText(this, "acceso no permitido", Toast.LENGTH_SHORT).show();
+        }
+        
+    }
+}
+
